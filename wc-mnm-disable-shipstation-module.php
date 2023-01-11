30251396568b95c929e700886271b283e972e76a
Kathy <507025+helgatheviking@users.noreply.github.com>
2023-01-11 10:24:21 -0700

    Update license to GPL3

--- wc-mnm-disable-shipstation-module.php
+++ wc-mnm-disable-shipstation-module.php
@@ -7,8 +7,8 @@
  * Version:           1.0.0
  * Author:            Kathy Darling
  * Author URI:        kathyisawesome.com
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
 // If this file is called directly, abort.
